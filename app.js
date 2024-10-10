2eb6dc7991b7b9aa254ea89b60da602e2496714f
Ayush Aryan <ayusharyan1309@gmail.com>
2024-10-11 00:11:23 +0530

    add vercel.json

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB and start the server
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
